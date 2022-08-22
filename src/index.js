cd1ac1f06acc6dea2241e4989f42648aaa4569e1
Jovan L <327527689@qq.com>
2022-08-22 16:25:07 +0800

    upgrade the versions for react and or dependencies

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './store';
 import { App } from './containers';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('app'));
+
+root.render(
     <Provider store={store}>
         <App />
     </Provider>, 
